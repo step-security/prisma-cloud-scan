7458d472b61d4a1444ff831514b0d148fbab96db
Raj Kumar Panda <rajkumar@stepsecurity.io>
2025-10-09 11:10:39 +0530

    onboarding prisma-cloud-scan action

--- README.md
+++ README.md
@@ -1,1 +1,91 @@
-# prisma-cloud-scan
+# Prisma Cloud Scan Action
+
+This GitHub Action will scan container images for vulnerabilities and compliance issues using Prisma Cloud by Palo Alto Networks. With it, you can receive immediate feedback about image vulnerabilities and compliance violations both in GitHub and in the Prisma Cloud Console as well as block builds that do not meet your compliance requirements, such as high or critical vulnerabilities.
+
+This action is a wrapper around [twistcli](https://docs.twistlock.com/docs/compute_edition/tools/twistcli_scan_images.html) which connects to the specified Prisma Cloud Console for vulnerability and compliance policy and metadata.
+
+## Usage
+### Example of container image scanning
+```yaml
+on: [ push, workflow_dispatch ]
+
+env:
+  IMAGE_NAME: ${{ github.repository }}:${{ github.sha }}
+
+jobs:
+  build-and-scan:
+    name: Build and scan image
+    runs-on: ubuntu-latest
+
+    steps:
+      - name: Check out the repository
+        uses: actions/checkout@v5
+
+      - name: Build the image
+        run: docker build -t $IMAGE_NAME .
+
+      - name: Prisma Cloud image scan
+        id: scan
+        uses: step-security/prisma-cloud-scan@v1
+        with:
+          pcc_console_url: ${{ secrets.PCC_CONSOLE_URL }}
+          pcc_user: ${{ secrets.PCC_USER }}
+          pcc_pass: ${{ secrets.PCC_PASS }}
+          image_name: ${{ env.IMAGE_NAME }}
+
+      # (Optional) for compatibility with GitHub's code scanning alerts
+      - name: Upload SARIF file
+        if: ${{ always() }} # necessary if using failure thresholds in the image scan
+        uses: github/codeql-action/upload-sarif@v2
+        with:
+          sarif_file: ${{ steps.scan.outputs.sarif_file }}
+```
+
+
+## Properties
+### Environment variables
+| Variable | Description | Required? | Default |
+|---|---|---|---|
+| `DOCKER_ADDRESS` | Address of the Docker daemon (e.g. tcp://localhost:2375) | No |  |
+| `DOCKER_HOST` | Address of the Docker daemon (e.g. tcp://localhost:2375). `DOCKER_ADDRESS` takes priority. | No |  |
+
+### Inputs
+| Input | Description | Required? | Default |
+|---|---|---|---|
+| `pcc_console_url` | URL of your Prisma Cloud Compute Console | Yes |  |
+| `pcc_user` | Username of a user with the CI user role | Yes |  |
+| `pcc_pass` | Password of a user with the CI user role | Yes |  |
+| `image_name` | Name (or ID) of the image to be scanned | Yes |  |
+| `project` | Tenant project context for the command | No |  |
+| `results_file` | File to which scan results are written in JSON | No | `pcc_scan_results.json` |
+| `sarif_file` | File to which scan results are written in SARIF | No | `pcc_scan_results.sarif.json` |
+| `docker_address` | Address of the Docker daemon (e.g. tcp://localhost:2375) | No |  |
+| `docker_tlscacert` | Path to the Docker CA certificate | No |  |
+| `docker_tlscert` | Path to the Docker client certificate | No |  |
+| `docker_tlskey` | Path to the Docker client private key | No |  |
+| `twistcli_debug` | Activate the debug flag for prisma cli (by default deactivated) | No |  |
+| `twistcli_publish` | Publish the results to Prisma Cloud. Default is true. | No |  |
+
+### Outputs
+| Output | Description |
+|---|---|
+| `results_file` | File to which scan results are written in JSON |
+| `sarif_file` | File to which scan results are written in SARIF |
+
+## Control alerting and fail thresholds
+You can set the level for alerting and failing builds in the Prisma Cloud UI. For example, setting the alert threshold to Medium will not alert for Low severity vulnerabilities.
+
+<img src="./images/pc_ci_rule_example.png">
+
+## Results output
+The table of discovered vulnerabilities and compliance violations shows up in the GitHub workflow log and in the Prisma Cloud Console in the Monitor section. If you [upload the outputted SARIF file to GitHub](https://docs.github.com/en/code-security/secure-coding/integrating-with-code-scanning/uploading-a-sarif-file-to-github), you will also populate the code scanning alerts section in your repository. While the code-scan-to-image-scan mapping isn't perfect, it does provide all available information of each vulnerability and compliance issue. The alerts will automatically close once the issues are fixed and the workflow is ran again.
+
+### GitHub workflow log
+<img src="./images/pc_github_log_output.png">
+
+### Prisma Cloud Console view
+<img src="./images/pc_ui_result.png">
+
+### GitHub code scanning alerts
+<img src="./images/pc_github_code_scanning.png">
+
---
+++ index.js
@@ -0,0 +1,367 @@
+const fs = require('fs');
+const axios = require('axios');
+const core = require('@actions/core');
+const { exec } = require('@actions/exec');
+const tc = require('@actions/tool-cache');
+const os = require('os');
+const { HttpsProxyAgent } = require('https-proxy-agent');
+
+const TRUE_VALUES = ['true', 'yes', 'y', '1'];
+
+function toSentenceCase(string) {
+  return string[0].toUpperCase() + string.slice(1).toLowerCase();
+}
+
+// https://github.com/nodejs/node/issues/18288#issuecomment-475864601
+// Necessary for Console URLs that already have a path (all SaaS Consoles)
+function joinUrlPath(...parts) {
+  // Filter handles cases where Console URL pathname is '/' in order to avoid '//api/v1/etc/' (double slash)
+  return '/' + parts.filter(part => part !== '/').map(part => part.replace(/(^\/|\/$)/g, '')).join('/');
+}
+
+// Wrapper around 'authenticate' Console API endpoint
+async function authenticate(url, user, pass, httpProxy) {
+  let parsedUrl;
+  try {
+    parsedUrl = new URL(url);
+  } catch (err) {
+    console.log(`Invalid Console address: ${url}`);
+    process.exit(1);
+  }
+  const endpoint = '/api/v1/authenticate';
+  parsedUrl.pathname = joinUrlPath(parsedUrl.pathname, endpoint);
+
+  try {
+    const res = await axios({
+      method: 'post',
+      url: parsedUrl.toString(),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      proxy: false,
+      httpsAgent: new HttpsProxyAgent(httpProxy),
+      data: {
+        username: user,
+        password: pass,
+      },
+    });
+    return res.data.token;
+  } catch (err) {
+    core.setFailed(`Failed getting authentication token: ${err.message}`);
+    process.exit(1);
+  }
+}
+
+// Wrapper around 'version' Console API endpoint
+async function getVersion(url, token, httpProxy) {
+  let parsedUrl;
+  try {
+    parsedUrl = new URL(url);
+  } catch (err) {
+    console.log(`Invalid Console address: ${url}`);
+    process.exit(1);
+  }
+  const endpoint = '/api/v1/version';
+  parsedUrl.pathname = joinUrlPath(parsedUrl.pathname, endpoint);
+
+  try {
+    const res = await axios({
+      method: 'get',
+      url: parsedUrl.toString(),
+      headers: {
+        'Authorization': `Bearer ${token}`,
+      },
+      proxy: false,
+      httpsAgent: new HttpsProxyAgent(httpProxy)
+    });
+    return res.data;
+  } catch (err) {
+    core.setFailed(`Failed getting version: ${err.message}`);
+    process.exit(1);
+  }
+}
+
+// GitHub Action-specific wrapper around 'util/twistcli' Console API endpoint
+// Saves twistcli using GitHub Action's tool-cache library
+async function getTwistcli(version, url, authToken, platform, architecture) {
+  let parsedUrl;
+  try {
+    parsedUrl = new URL(url);
+  } catch (err) {
+    console.log(`Invalid Console address: ${url}`);
+    process.exit(1);
+  }
+  let endpointPrefix = '';
+  let endpointValue = 'twistcli';
+  
+  if(platform === 'win32') {
+    endpointPrefix = 'windows/';
+    endpointValue = 'twistcli.exe';
+  } else if(platform === 'darwin') {
+    endpointPrefix = 'osx/';
+    if (architecture === 'arm64') endpointPrefix += 'arm64/';
+  } else if(platform === 'linux') {
+    if (architecture === 'arm64') endpointPrefix += 'arm64/';
+  }
+
+  parsedUrl.pathname = joinUrlPath(parsedUrl.pathname, '/api/v1/util/' + endpointPrefix + endpointValue);
+
+  let twistcli = tc.find('twistcli', version);
+  if (!twistcli) {
+    const twistcliPath = await tc.downloadTool(parsedUrl.toString(), undefined, `Bearer ${authToken}`);
+    if (platform !== 'win32') await exec(`chmod a+x ${twistcliPath}`);
+    twistcli = await tc.cacheFile(twistcliPath, endpointValue, 'twistcli', version);
+  }
+  core.addPath(twistcli);
+
+  return endpointValue;
+}
+
+function formatSarifToolDriverRules(results) {
+  // Only 1 image can be scanned at a time
+  const result = results[0];
+  const vulnerabilities = result.vulnerabilities;
+  const compliances = result.compliances;
+
+  const vulnerabilitiesFiltered = (vulnerabilities || []).filter(
+    (thing, index, self) =>
+      index ===
+      self.findIndex((t) => t.id === thing.id )
+  ); 
+
+
+  let vulns = [];
+  if (vulnerabilitiesFiltered) {
+    vulns = vulnerabilitiesFiltered.map(vuln => {
+      return {
+        id: `${vuln.id}`,
+        shortDescription: {
+          text: `[Prisma Cloud] ${vuln.id} in ${vuln.packageName} (${vuln.severity})`,
+        },
+        fullDescription: {
+          text: `${toSentenceCase(vuln.severity)} severity ${vuln.id} found in ${vuln.packageName} version ${vuln.packageVersion}`,
+        },
+        help: {
+          text: '',
+          markdown: '| CVE | Severity | CVSS | Package | Version | Fix Status | Published | Discovered |\n' +
+            '| --- | --- | --- | --- | --- | --- | --- | --- |\n' +
+            '| [' + vuln.id + '](' + vuln.link + ') | ' + vuln.severity + ' | ' + (vuln.cvss || 'N/A') + ' | ' + vuln.packageName + ' | ' + vuln.packageVersion + ' | ' + (vuln.status || 'not fixed') + ' | ' + vuln.publishedDate + ' | ' + vuln.discoveredDate + ' |',
+        },
+      };
+    });
+  }
+
+  let comps = [];
+  if (compliances) {
+    comps = compliances.map(comp => {
+      return {
+        id: `${comp.id}`,
+        shortDescription: {
+          text: `[Prisma Cloud] Compliance check ${comp.id} violated (${comp.severity})`,
+        },
+        fullDescription: {
+          text: `${toSentenceCase(comp.severity)} severity compliance check "${comp.title}" violated`,
+        },
+        help: {
+          text: '',
+          markdown: '| Compliance Check | Severity | Title |\n' +
+            '| --- | --- | --- |\n' +
+            '| ' + comp.id + ' | ' + comp.severity + ' | ' + comp.title + ' |',
+        },
+      };
+    });
+  }
+
+  return [...vulns, ...comps];
+}
+
+/**
+ * convert prima severity to github severity
+ * @param {string} severity
+ * @throws {Error} unknown severity
+ * @returns string
+ */
+function convertPrismaSeverity(severity) {
+  // prisma: critical, high, important, medium, moderate, unimportant, low
+  // gh: error, warning, note, none
+  switch (severity) {
+    case "critical":
+      return "error";
+    case "high":
+      return "warning";
+    case "important":
+      return "warning";
+    case "medium":
+      return "note";
+    case "moderate":
+      return "note";
+    case "low":
+      return "none";
+    case "unimportant":
+    case "negligible":
+    case "unreviewed":
+    case "unassigned": 
+      return "none";
+    default:
+      throw new Error(`Unknown severity: ${severity}`);
+  }
+}
+
+
+function formatSarifResults(results) {
+  // Only 1 image can be scanned at a time
+  const result = results[0];
+  const imageName = result.name;
+  let findings = [];
+  if (result.vulnerabilities) {
+    findings = [...findings, ...result.vulnerabilities];
+  }
+  if (result.compliances) {
+    findings = [...findings, ...result.compliances];
+  }
+
+  if (findings) {
+    return findings.map(finding => {
+      return {
+        ruleId: `${finding.id}`,
+        level: `${convertPrismaSeverity(finding.severity)}`,
+        message: {
+          text: `Description:\n${finding.description}`,
+        },
+        locations: [{
+          physicalLocation: {
+            artifactLocation: {
+              uri: `${imageName}`,
+            },
+            region: {
+              startLine: 1,
+              startColumn: 1,
+              endLine: 1,
+              endColumn: 1,
+            },
+          },
+        }],
+      };
+    });
+  }
+
+  return [];
+}
+
+function formatSarif(twistcliVersion, resultsFile) {
+  try {
+    const scan = JSON.parse(fs.readFileSync(resultsFile, 'utf8'));
+    const sarif = {
+      $schema: 'https://raw.githubusercontent.com/oasis-tcs/sarif-spec/master/Schemata/sarif-schema-2.1.0.json',
+      version: '2.1.0',
+      runs: [{
+        tool: {
+          driver: {
+            name: 'Prisma Cloud (twistcli)',
+            version: `${twistcliVersion}`,
+            rules: formatSarifToolDriverRules(scan.results),
+          },
+        },
+        results: formatSarifResults(scan.results),
+      }],
+    };
+    return sarif;
+  } catch (err) {
+    core.setFailed(`Failed formatting SARIF: ${err.message}`);
+    process.exit(1);
+  }
+}
+
+async function scan() {
+  const httpProxy = process.env.https_proxy || process.env.HTTPS_PROXY || process.env.http_proxy || process.env.HTTP_PROXY;
+  const consoleUrl = core.getInput('pcc_console_url');
+  const username = core.getInput('pcc_user');
+  const password = core.getInput('pcc_pass');
+  const imageName = core.getInput('image_name');
+  const containerized = core.getInput('containerized').toLowerCase();
+  const dockerAddress = core.getInput('docker_address') || process.env.DOCKER_ADDRESS || process.env.DOCKER_HOST;
+  const dockerTlsCaCert = core.getInput('docker_tlscacert');
+  const dockerTlsCert = core.getInput('docker_tlscert');
+  const dockerTlsKey = core.getInput('docker_tlskey');
+  const project = core.getInput('project');
+  const twistcli_debug = core.getInput('twistcli_debug');
+  const twistcli_publish = core.getInput('twistcli_publish');
+  const resultsFile = core.getInput('results_file');
+  const sarifFile = core.getInput('sarif_file');
+
+  try {
+    let token;
+    try {
+      token = await authenticate(consoleUrl, username, password, httpProxy);
+    } catch (err) {
+      core.setFailed(`Failed authenticating: ${err.message}`);
+      process.exit(1);
+    }
+
+    let twistcliVersion;
+    try {
+      twistcliVersion = await getVersion(consoleUrl, token, httpProxy);
+    } catch (err) {
+      core.setFailed(`Failed getting version: ${err.message}`);
+      process.exit(1);
+    }
+    twistcliVersion = twistcliVersion.replace(/"/g, '');
+
+    let twistcliCmd = await getTwistcli(twistcliVersion, consoleUrl, token, os.platform(), os.arch());
+    twistcliCmd = [twistcliCmd];
+    if (httpProxy) {
+      twistcliCmd = twistcliCmd.concat([`--http-proxy ${httpProxy}`]);
+    }
+    if (TRUE_VALUES.includes(twistcli_debug)) {
+      twistcliCmd = twistcliCmd.concat(['--debug']);
+    }
+    twistcliCmd = twistcliCmd.concat([
+      'images', 'scan',
+      `--address ${consoleUrl}`,
+      `--user ${username}`, `--password ${password}`,
+      `--output-file ${resultsFile}`,
+      '--details',
+    ]);
+    if (dockerAddress) {
+      twistcliCmd = twistcliCmd.concat([`--docker-address ${dockerAddress}`]);
+    }
+    if (dockerTlsCaCert) {
+      twistcliCmd = twistcliCmd.concat([`--docker-tlscacert ${dockerTlsCaCert}`]);
+    }
+    if (dockerTlsCert) {
+      twistcliCmd = twistcliCmd.concat([`--docker-tlscert ${dockerTlsCert}`]);
+    }
+    if (dockerTlsKey) {
+      twistcliCmd = twistcliCmd.concat([`--docker-tlskey ${dockerTlsKey}`]);
+    }
+    if (project) {
+      twistcliCmd = twistcliCmd.concat([`--project ${project}`]);
+    }
+    if (TRUE_VALUES.includes(containerized)) {
+      twistcliCmd = twistcliCmd.concat(['--containerized']);
+    }
+    if (twistcli_publish) {
+      twistcliCmd = twistcliCmd.concat([`--publish=${twistcli_publish}`]);
+    }
+
+    twistcliCmd = twistcliCmd.concat([imageName]);
+    const exitCode = await exec(twistcliCmd.join(' '), undefined, {
+      ignoreReturnCode: true,
+    });
+    if (exitCode > 0) {
+      core.setFailed('Image scan failed');
+    }
+
+    fs.writeFileSync(sarifFile, JSON.stringify(formatSarif(twistcliVersion, resultsFile)));
+
+    core.setOutput('results_file', resultsFile);
+    core.setOutput('sarif_file', sarifFile);
+  } catch (err) {
+    core.setFailed(`Image scan failed: ${err.message}`);
+    process.exit(1);
+  }
+}
+
+if (require.main === module) {
+  scan().catch((err) => core.setFailed(err.message));
+}
